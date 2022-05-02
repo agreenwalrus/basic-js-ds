e6614eb57783c76fafae9452069e87da960647b5
Yuliya Runova <jul.runova@gmail.com>
2022-05-02 17:21:14 +0400

    feat: tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,39 +8,85 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
+  _root = null;
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._root;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    let node = new Node(data);
+    let pointer = this._root;
+    let parent = this._root;
+    while(pointer != null) {
+        parent = pointer;
+        if(data <= pointer.data)
+          pointer = pointer.left;
+        else pointer = pointer.right;
+    } 
+    if (parent === null)
+      this._root = node;
+    else if (data <= parent.data)
+      parent.left = node;
+    else
+      parent.right = node;
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return this.find(data) != null;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let pointer = this._root;
+    while(pointer && pointer.data != data) {
+        if(data < pointer.data)
+          pointer = pointer.left;
+        else pointer = pointer.right;
+    }
+    return pointer;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    let pointer = this._root;
+    let parent = this._root;
+    while(pointer && pointer.data != data) {
+        parent = pointer;
+        if(data < pointer.data)
+          pointer = pointer.left;
+        else pointer = pointer.right;
+    } 
+    let rightPointer = pointer.right;
+    if (rightPointer) {
+      while(rightPointer.left) {
+        rightPointer = rightPointer.left;
+      }
+      rightPointer.left = pointer.left;
+    } else {
+      pointer.right = pointer.left;
+    }
+    if (pointer === this._root) {
+      this._root = pointer.right;
+    } else if (pointer === parent.left) {
+      parent.left = pointer.right;
+    }
+    else if (pointer === parent.right) {
+      parent.right = pointer.right;
+    };
+    
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let pointer = this._root;
+    while (pointer.left != null)
+      pointer = pointer.left;
+    return pointer.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let pointer = this._root;
+    while (pointer.right != null)
+      pointer = pointer.right;
+    return pointer.data;
   }
 }
 
